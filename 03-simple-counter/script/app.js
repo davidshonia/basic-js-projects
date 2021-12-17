5eb404a5334e1a82cf06b31a08250664b16149d0
davidshonia <dupecube1@gmail.com>
2021-12-17 20:04:35 +0400

    counter project

---
+++ 03-simple-counter/script/app.js
@@ -0,0 +1,23 @@
+let count = 0;
+
+
+//selet value and button
+
+const value = document.querySelector('#value');
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(function(eachButton){
+    console.log(eachButton)
+    let color 
+    eachButton.addEventListener('click', function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        }else if(styles.contains('increase')){
+            count++;
+        }else{
+            count=0;
+        }
+        return value.textContent = count;
+    })
+})
